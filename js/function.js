2c9c0a265868f0521d71a6f4d303e8f0230985df
Daniela Zepeda <zepeda218@hotmail.com>
2018-02-25 13:25:28 -0600

    version1

---
+++ js/function.js
@@ -0,0 +1,17 @@
+$(function () {
+    $('form').submit(function (event) { 
+    var input = $(this).children('.password').val();
+
+    var password = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$&+,:;=?@#|'<>.^*()%!-])(?!.*\s).{8,}$/);
+
+    if (!password.test(input)) {  
+     $(".alerthidden").removeClass('fade'); 
+     $(".alerthidden").addClass('show'); 
+     event.preventDefault(event);   
+    }
+    else{ 
+     $(".alerthidden").addClass('fade'); 
+     $(".alerthidden").removeClass('show'); 
+    }
+    });
+   });
