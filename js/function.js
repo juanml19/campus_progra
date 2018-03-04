f6913dbf7385dc2a5e5bfcf65b14806d624698b8
Daniela Zepeda <zepeda218@hotmail.com>
2018-03-04 11:56:52 -0600

    Automatic changes

--- js/function.js
+++ js/function.js
@@ -1,17 +1,4 @@
-//js login
-function validateLogin(){
-    var cellEmpty = true;
-    jQuery("input[type='text']:not(.HiddenIndex)").each(function(){
-        if(jQuery(this).val().trim() == ""){
-            jQuery(this).addClass("Error");
-            cellEmpty= false ;
-        }else{
-            jQuery(this).removeClass("Error");
-        }
-    });
-    return  cellEmpty;
-}
-
+//---------------------------------
 jQuery(document).ready(function () {
     jQuery('.dataTable').DataTable( {
         "language": {
@@ -158,25 +145,6 @@ function SoloNumeros(evt){
 			}
 			else{ console.log ("vacio")}
 		}
-
-
-$(function () {
-    $('.form-login').submit(function (event) { 
-    var input = $(this).children('.password').val();
-        console.log(input);
-    var password = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$&+,:;=?@#|'<>.^*()%!-])(?!.*\s).{8,}$/);
-
-    if (!password.test(input)) {  
-     $(".alerthidden").removeClass('fade'); 
-     $(".alerthidden").addClass('show'); 
-     event.preventDefault(event);   
-    }
-    else{ 
-     $(".alerthidden").addClass('fade'); 
-     $(".alerthidden").removeClass('show'); 
-    }
-    });
-   });
 
 
 //asistencia
