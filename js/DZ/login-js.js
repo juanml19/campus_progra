a3315b939681ab1123b994102c7fee05846b2f43
Daniela Zepeda <zepeda218@hotmail.com>
2018-03-11 10:35:49 -0600

    Relocating carpets

--- js/DZ/login-js.js
+++ js/DZ/login-js.js
@@ -1,23 +1,12 @@
-//js login
-function validateLogin(){
-    var cellEmpty = true;
-    jQuery("input[type='text']:not(.HiddenIndex)").each(function(){
-        if(jQuery(this).val().trim() == ""){
-            jQuery(this).addClass("Error");
-            cellEmpty= false ;
-        }else{
-            jQuery(this).removeClass("Error");
-        }
-    });
-    return  cellEmpty;
-}
-
+//password format
 $(function () {
     $('.form-login').submit(function (event) { 
+        var input2 = $(this).children('.usuario').val();
+        console.log(input2);
+        $.trim(".usuario.html()");
     var input = $(this).children('.password').val();
         console.log(input);
     var password = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$&+,:;=?@#|'<>.^*()%!-])(?!.*\s).{8,}$/);
-
     if (!password.test(input)) {  
      $(".alerthidden").removeClass('fade'); 
      $(".alerthidden").addClass('show'); 
@@ -28,5 +17,9 @@ $(function () {
      $(".alerthidden").removeClass('show'); 
     }
     });
+    
+    
+    
    });
+
 
