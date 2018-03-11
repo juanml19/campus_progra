629aab543213ab15a2998212e4f534bc020f1037
Alfredo Valverde <chinochinese2@gmail.com>
2018-03-11 15:08:32 -0600

    Cambios de validacion

--- js/FormCarrera.js
+++ js/FormCarrera.js
@@ -26,18 +26,42 @@
 });
 
 (function () {
-    $('form').submit(function (event) {
+    $('.form-nuevaCarrera').submit(function (event) {
         var mensaje = "";
-        $('.validate').each(function () {
+        $('.form-nuevaCarrera .validate').each(function () {
+			console.log($(this).val().trim());
+			console.log($(this));
             if ($(this).val().trim().length < 5) {
                 var label = $('label', this.parentElement).text();
                 mensaje += "\n"+label.substring(0, label.length - 1);
             }
         });
+		console.log(mensaje);
         if (mensaje != "") {
             alert("Los siguiente campos tienen menos de 5 characteres:\n"+mensaje);
             event.preventDefault(event);
         }
+		
+    });
+})();
+
+(function () {
+    $('.form-editarCarrera').submit(function (event) {
+        var mensaje = "";
+        $('.form-editarCarrera .validate').each(function () {
+			console.log($(this).val().trim());
+			console.log($(this));
+            if ($(this).val().trim().length < 5) {
+                var label = $('label', this.parentElement).text();
+                mensaje += "\n"+label.substring(0, label.length - 1);
+            }
+        });
+		console.log(mensaje);
+        if (mensaje != "") {
+            alert("Los siguiente campos tienen menos de 5 characteres:\n"+mensaje);
+            event.preventDefault(event);
+        }
+		
     });
 })();
 
