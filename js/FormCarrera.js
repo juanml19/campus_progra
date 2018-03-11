f998391867ef21ba0df97b147c893945d23a3b7f
Alfredo Valverde <chinochinese2@gmail.com>
2018-03-11 14:30:13 -0600

    Arreglos a vista Carrera
    cambios en el js y css

--- js/FormCarrera.js
+++ js/FormCarrera.js
@@ -1,4 +1,31 @@
-﻿(function () {
+﻿$(document).ready(function () {
+    $('.dataTable').DataTable( {
+        "language": {
+            "url": "https://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json"
+        },
+        responsive: true
+    });
+    $('.not-number').keyup(function (e) {
+        if (!/^[ a-záéíóúüñ]*$/i.test(this.value)) {
+            console.log(this.value);
+            this.value = this.value.replace(/[^ a-záéíóúüñ]+/ig,"");
+        }
+    });
+    $('.no-espacios').keyup(function (e) {
+        var value = $(this).val(); 
+        if (value == " ") {
+            $(this).val("");
+        }
+    });
+    $(".editar-carrera").click(function(){
+       $(".titulo-form-carrera").html("Editar carrera"); 
+    });
+    $(".nueva-carrera").click(function(){
+       $(".titulo-form-carrera").html("Nueva carrera"); 
+    });
+});
+
+(function () {
     $('form').submit(function (event) {
         var mensaje = "";
         $('.validate').each(function () {
@@ -17,3 +44,4 @@
 function myFunctionDelete() {
     alert("Se borrará la Carrera");
 }
+
